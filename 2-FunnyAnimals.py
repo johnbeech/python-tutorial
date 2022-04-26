0acb8fa3d5fe5753e29e4a0de16a7d3b7b35bf93
John Beech <john.beech@netacea.com>
2022-04-26 09:31:58 +0100

    Complete challenge 1 on https://www.csnewbs.com/ks3-python-2-variables

---
+++ 2-FunnyAnimals.py
@@ -0,0 +1,9 @@
+# John Beech / Tuesday 26th April 2022
+# Create a variable for a colour and give it a value (e.g. "blue")
+# Create a variable for an animal and give it a value (e.g. "horse")
+# Print a funny sentence that uses both variables.
+# BONUS: Try to use only one print line.
+# BONUS: Try to use only three lines in total.
+colour = "red"
+animal = "elk"
+print("The", animal, "was frolicking around in the forest, when it bumped into a tree full of", colour, "berries!\nThe", animal, "then got covered in", colour, "splotches of juice as the berries exploded!")
