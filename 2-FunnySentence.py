1134e2db4c0b9f65816d946429fc96fea4ff6ea3
John Beech <john.beech@netacea.com>
2022-04-26 09:35:09 +0100

    Complete challenge 2 on https://www.csnewbs.com/ks3-python-2-variables

---
+++ 2-FunnySentence.py
@@ -0,0 +1,9 @@
+# John Beech / Tuesday 26th April 2022
+# Write a program that uses three variables, an adjective (descriptive word), a number and an animal. Print a funny response using all variables.
+# BONUS: Try to use only one print line.
+# BONUS: Try to use only four lines in total.
+adjective = "hungry"
+number = "125"
+animal = "panda"
+
+print("The", adjective, animal, "ate", number, "shoots of bamboo on a rainy day!")
