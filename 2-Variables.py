2a3c0075043b7c267c737dc40170c7c6a0c4dd02
John Beech <john.beech@netacea.com>
2022-04-26 09:27:47 +0100

    Complete https://www.csnewbs.com/ks3-python-2-variables Steps 1-4

---
+++ 2-Variables.py
@@ -0,0 +1,8 @@
+# John Beech / Tuesday 26th April 2022
+sweets = 15
+print("There are", sweets, "sweets")
+
+name = "John"
+print("My name is", name)
+
+print("I am", name, "and I will eat all", sweets, "sweets!")
