fea124c0f47c180ac723ad5100da91f6fdedb174
John Beech <john.beech@netacea.com>
2022-04-26 09:48:20 +0100

    Complete challenge 1 on https://www.csnewbs.com/ks3-python-3-inputs

---
+++ 3-FunnyFood.py
@@ -0,0 +1,7 @@
+# John Beech / Tuesday 26th April 2022
+# Create a program that asks two questions, one for their favourite colour and one for their favourite food.
+# Print a funny sentence using both of their answers.
+# BONUS: Try to use only one print line.
+favouriteColour = input("What is your favourite colour? ")
+favouriteFood = input("What is your favourite food? ")
+print("Oh I love", favouriteFood, "when its mixed with", favouriteColour, "mash potato! Delicious", favouriteColour, favouriteFood, "mashed potato!")
